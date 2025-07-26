ec6a2fe99ea947f2056f62288a5eeaef32168cf4
Lindy_Green <rvhovhannisyan@outlook.com>
2025-07-27 02:24:32 +0400

    Removed Redundant code for grid debugging.

--- Source/GASLearning/Public/StructsAndEnums/E_TileState.h
+++ Source/GASLearning/Public/StructsAndEnums/E_TileState.h
@@ -11,10 +11,6 @@ enum class ETileState : uint8
 	None         UMETA(DisplayName = "None"),
 	Hovered      UMETA(DisplayName = "Hovered"),
 	Selected     UMETA(DisplayName = "Selected"),
-	Neighbor     UMETA(DisplayName = "Neighbor"),
-	IsInPath     UMETA(DisplayName = "Is In Path"),
-	IsDiscovered UMETA(DisplayName = "Is Discovered"),
-	IsAnalyzed   UMETA(DisplayName = "Is Analyzed"),
 	IsReachable  UMETA(DisplayName = "Is Reachable"),
 	IsInRange    UMETA(DisplayName = "Is In Range"),
 	IsSpellAOE   UMETA(DisplayName = "Is Spell AOE")
